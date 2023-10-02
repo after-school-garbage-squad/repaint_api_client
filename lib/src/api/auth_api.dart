2064f14a592e685c589fd4a0ef93e8175f59c779
Hideki Tanaka <work@tuxsnct.com>
2023-10-02 17:41:40 +0900

    feat: update schema

--- lib/src/api/auth_api.dart
+++ lib/src/api/auth_api.dart
@@ -6,13 +6,11 @@ import 'dart:async';
 
 // ignore: unused_import
 import 'dart:convert';
-import 'package:repaint_api_client/src/deserialize.dart';
 import 'package:dio/dio.dart';
 
 import 'package:repaint_api_client/src/model/register_request.dart';
 
 class AuthApi {
-
   final Dio _dio;
 
   const AuthApi(this._dio);
@@ -21,7 +19,7 @@ class AuthApi {
   /// 管理者を作成し、システムに登録する
   ///
   /// Parameters:
-  /// * [registerRequest] 
+  /// * [registerRequest]
   /// * [cancelToken] - A [CancelToken] that can be used to cancel the operation
   /// * [headers] - Can be used to add additional headers to the request
   /// * [extras] - Can be used to add flags to the request
@@ -31,7 +29,7 @@ class AuthApi {
   ///
   /// Returns a [Future]
   /// Throws [DioException] if API call or serialization fails
-  Future<Response<void>> register({ 
+  Future<Response<void>> register({
     required RegisterRequest registerRequest,
     CancelToken? cancelToken,
     Map<String, dynamic>? headers,
@@ -57,10 +55,10 @@ class AuthApi {
     dynamic _bodyData;
 
     try {
-_bodyData=jsonEncode(registerRequest);
-    } catch(error, stackTrace) {
+      _bodyData = jsonEncode(registerRequest);
+    } catch (error, stackTrace) {
       throw DioException(
-         requestOptions: _options.compose(
+        requestOptions: _options.compose(
           _dio.options,
           _path,
         ),
@@ -81,5 +79,4 @@ _bodyData=jsonEncode(registerRequest);
 
     return _response;
   }
-
 }
